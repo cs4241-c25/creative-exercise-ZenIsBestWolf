9d7701551f7e82f8615f5525de2c9a1582e068f9
Zen Dignan <zen@dignan.dev>
2025-02-15 15:27:23 -0500

    oops lol

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
-const http = require('http');
+"use strict";
+
+import mime from 'mime';
+import http from 'http';
+import fs from 'fs';
+
 const port = 8080;
 
 const server = http.createServer((request, response) => {
@@ -16,8 +21,29 @@ const handleGet = (request, response) => {
         sendFile(response, "public/index.html");
         return;
     }
-    response.writeHead(404);
-    response.end("404: Not Found");
+    const filename = "public/" + request.url.slice(1);
+    sendFile(response, filename);
+};
+
+const sendFile = (response, filename) => {
+    const type = mime.getType(filename);
+    if (type === null) {
+        console.error("No type found");
+        return;
+    }
+    fs.readFile(filename, (err, content) => {
+        // if the error = null, then we've loaded the file successfully
+        if (err === null) {
+            // status code: https://httpstatuses.com
+            response.writeHead(200, 'OK', { "content-type": type });
+            response.end(content);
+        }
+        else {
+            // file not found, error code 404
+            response.writeHead(404);
+            response.end("404: File Not Found");
+        }
+    });
 };
 
 server.listen(process.env.PORT || port);
